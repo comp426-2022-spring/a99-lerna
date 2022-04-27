abd2638bba4dc76110e6d776f27abe55005cc55f
lukeduck <lukeduck@live.unc.edu>
2022-04-27 09:49:34 -0400

    basic login (non-functional)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Login from "./components/Login.js"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Health Tracking App</h1>
-    </div>
+
+    <Login/>
+
   );
 }
 
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Login() {
+    return(
+    <div>
+        <title>
+            Login Page
+        </title>
+        <form id = "usernameinput">
+            <label for="username">
+                <strong>
+                    Username:
+                </strong>
+                <input type="text" id="username"></input>
+            </label>
+        </form>
+        <form id = "passwordinput">
+            <label for="password">
+                <strong>
+                    Password:
+                </strong>
+                <input type="text" id="password"></input>
+            </label>
+        </form>
+        <input type="submit" value="Login"></input>
+        <input type="submit" value="Create Account"></input>
+    </div>
+    )
+}
+
+export default Login
