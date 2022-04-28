6cb3489a982b812ea3f7e98ea7f4d434758fea1b
“Terrence <tjkapur@live.unc.edu>
2022-04-27 20:36:27 -0400

    test data'

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react';
 import './App.css';
 import Login from "./components/Login.js"
 import Create from "./components/new-user.js"
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/app/user/user1")
+    .then((res) => res.json())
+    .then((data) => setData(data.weight));
+  }, []);
+
   return (
-   <Login/>
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+   
   );
 }
 
