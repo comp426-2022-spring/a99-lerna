03c81d92a1ba1a4ed853ee159bd734be2e17972b
“Terrence <tjkapur@live.unc.edu>
2022-04-27 20:43:31 -0400

    retrieval from api

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,27 @@ import Create from "./components/new-user.js"
 function App() {
 
   const [data, setData] = React.useState(null);
+  const [data2, setData2] = React.useState(null);
 
   React.useEffect(() => {
     fetch("/app/user/user1")
     .then((res) => res.json())
-    .then((data) => setData(data.weight));
+    .then((data) => setData( data.weight));
+
+  }, []);
+
+  React.useEffect(() => {
+    fetch("/app/user/user1")
+    .then((res) => res.json())
+    .then((data2) => setData2( data2.height));
+
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <p>{!data ? "Loading..." : data}</p>
+        <p>{!data2 ? "Loading..." : data2}</p>
       </header>
     </div>
    
